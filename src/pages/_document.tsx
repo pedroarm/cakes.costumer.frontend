3aeac8acbff06f6a38d26c7ff3265f07dbe0cd41
pedroarm <p.augusto.rib@gmail.com>
2022-05-04 20:40:36 -0300

    fix: styled ssg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import Document, {
+  DocumentInitialProps,
   DocumentContext,
   Html,
   Head,
   Main,
   NextScript
 } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    return initialProps
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ]
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render(): JSX.Element {
